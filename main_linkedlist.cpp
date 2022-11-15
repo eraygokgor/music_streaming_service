e21def37186eb3aeb3da9aadaefdeef336bb2dc3
eraygokgor <gokgoreray@gmail.com>
2022-11-15 22:43:19 +0300

    update gitignore

--- main_linkedlist.cpp
+++ main_linkedlist.cpp
@@ -4,17 +4,25 @@
 
 int main() {
     LinkedList<int> llist;
+    
 
-    std::cout<<llist.isEmpty()<<std::endl;
-
+    llist.insertAtTheEnd(0); //0
+    llist.insertAtTheEnd(1); //1
+    llist.insertAtTheEnd(2); //2
+    llist.insertAtTheEnd(3); //3
+    llist.insertAtTheEnd(4); //4
+    std::cout<<"Initial Size: "<<llist.getSize()<<std::endl;
+    
     /*
-    llist.print();
-
-    llist.insertAtTheEnd(3);
-    llist.insertAtTheFront(2);
-    llist.insertAfterNode(1, llist.getNode(2));
-
-    llist.print();
+    Node<int> *node1 = llist.getNodeAtIndex(0);
+    Node<int> *node2 = llist.getNodeAtIndex(0);
+    llist.swap(node1, node2);
+    llist.shuffle(42);
     */
+    LinkedList<int> llist_copy(llist);
+
+    std::cout<<"Size: "<<llist_copy.getSize()<<std::endl;
+    llist_copy.print();
+
     return 0;
 }
